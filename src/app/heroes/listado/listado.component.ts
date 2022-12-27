69aa908c08849ac8300d482823f5186645286f6e
Rafael Uribe <72708040+rafaelUribe@users.noreply.github.com>
2022-12-27 16:25:22 -0600

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán América'];
+
+  heroeBorrado: string = "";
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+
+}
