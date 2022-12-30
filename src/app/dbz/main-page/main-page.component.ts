49809efa1f1dc15b2914b6789b6372490071e643
Jesus Rafael Uribe Gallardo <m1juribega@oreillyauto.mx>
2022-12-30 10:04:54 -0600

    refactored, service implemented

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  constructor(){}
 
 }
